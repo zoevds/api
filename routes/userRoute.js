dbf05ad90a71dbef794834201dda79bf9806d8e7
zoevds <zoevanderschyff13@gmail.com>
2022-07-27 14:30:17 +0200

    first commit

---
+++ routes/userRoute.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const { route } = require("express/lib/application");
+const router = express.Router();
+const con = require("../lib/dbConnection");
+
+router.get("/", (req, res) => {
+  try {
+    con.query("SELECT * FROM users", (err, result) => {
+      if (err) throw err;
+      res.send({ id: req.params.id });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+// Gets one users
+router.get("/:id", (req, res) => {
+  try {
+    res.send({ id: req.params.id });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+router.post("/", (req, res) => {
+  const {
+    email,
+    password,
+    full_name,
+    billing_address,
+    default_shipping_address,
+    country,
+    phone,
+    user_type,
+  } = req.body;
+  try {
+    con.query(
+      `insert into users (email,password,full_name,billing_address,default_shipping_address,country,phone, user_type) values ('${email}','${password}', '${full_name}','${billing_address}','${default_shipping_address}', '${country}', '${phone}','${user_type}')`,
+      (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
