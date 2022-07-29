6d015cb70e5020da4baa9580686e8b65137029a2
zoevds <zoevanderschyff13@gmail.com>
2022-07-29 12:18:08 +0200

    updates

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,9 +2,12 @@ const express = require("express");
 const { route } = require("express/lib/application");
 const router = express.Router();
 const con = require("../lib/dbConnection");
+const jwt = require("jsonwebtoken");
+const middleware = require("../middleware/auth");
+const bcrypt = require("bcryptjs");
 
 // GET ALL USERS
-router.get("/", (req, res) => {
+router.get("/", middleware, (req, res) => {
   try {
     con.query("SELECT * FROM users", (err, result) => {
       if (err) throw err;
@@ -66,6 +69,8 @@ router.put("/:id", (req, res) => {
     phone,
     user_type,
   } = req.body;
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
   try {
     con.query(
       `UPDATE users SET email="${email}",password="${password}",full_name="${full_name}",billing_address="${billing_address}",default_shipping_address="${default_shipping_address}",country="${country}",phone= "${phone}", user_type="${user_type}" WHERE user_id="${req.params.id}"`,
@@ -93,4 +98,112 @@ router.delete("/:id", (req, res) => {
     console.log(error);
   }
 });
+
+// REGISTER ROUTE
+// The Route where Encryption starts
+router.post("/register", (req, res) => {
+  try {
+    let sql = "INSERT INTO users SET ?";
+    const {
+      full_name,
+      email,
+      password,
+      user_type,
+      phone,
+      country,
+      billing_address,
+      default_shipping_address,
+    } = req.body;
+
+    // The start of hashing / encryption
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    let user = {
+      full_name,
+      email,
+      // We sending the hash value to be stored witin the table
+      password: hash,
+      user_type,
+      phone,
+      country,
+      billing_address,
+      default_shipping_address,
+    };
+    con.query(sql, user, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      res.send(`User ${(user.full_name, user.email)} created successfully`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// LOGIN
+router.post("/login", (req, res) => {
+  try {
+    let sql = "SELECT * FROM users WHERE ?";
+    let user = {
+      email: req.body.email,
+    };
+    con.query(sql, user, async (err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+        res.send("Email not found please register");
+      } else {
+        const isMatch = await bcrypt.compare(
+          req.body.password,
+          result[0].password
+        );
+        if (!isMatch) {
+          res.send("Password incorrect");
+        } else {
+          // The information the should be stored inside token
+          const payload = {
+            user: {
+              user_id: result[0].user_id,
+              full_name: result[0].full_name,
+              email: result[0].email,
+              user_type: result[0].user_type,
+              phone: result[0].phone,
+              country: result[0].country,
+              billing_address: result[0].billing_address,
+              default_shipping_address: result[0].default_shipping_address,
+            },
+          };
+          // Creating a token and setting expiry date
+          jwt.sign(
+            payload,
+            process.env.jwtSecret,
+            {
+              expiresIn: "365d",
+            },
+            (err, token) => {
+              if (err) throw err;
+              res.json({ token });
+            }
+          );
+        }
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+// VERIFY
+router.get("/users/verify", (req, res) => {
+  const token = req.header("x-auth-token");
+  jwt.verify(token, process.env.jwtSecret, (error, decodedToken) => {
+    if (error) {
+      res.status(401).json({
+        msg: "Unauthorized Access!",
+      });
+    } else {
+      res.status(200);
+      res.send(decodedToken);
+    }
+  });
+});
+
 module.exports = router;
