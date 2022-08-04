ccc9e6deabb28e43cc3e6384f53c6d894f99f179
zoevds <zoevanderschyff13@gmail.com>
2022-08-04 09:06:38 +0200

    added static

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 const express = require("express"); // Used to set up a server
 const cors = require("cors"); // Used to prevent errors when working locally
 
+//LOGIN
+// REGISTER
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcryptjs");
+// const con = require("../lib/dbConnection");
+// require("dotenv").config();
+
+// async function Login(req, res) {}
 // Import routes
 const userRoute = require("./routes/userRoute");
 const productsRoute = require("./routes/productsRoute");
@@ -14,6 +22,10 @@ const app = express(); // Initialize express as an app variable
 app.set("port", process.env.PORT || 6969); // Set the port
 app.use(express.json()); // Enable the server to handle JSON requests
 app.use(cors()); // Dont let local development give errors
+
+app.use(express.static("public"));
+//Serves all the request which includes /images in the url from Images folder
+app.use("/index.html", express.static(__dirname + "/index.html"));
 
 // This is where we check URLs and Request methods to create functionality
 // GET '/' is always what will be displayed on the home page of your application
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,9 +5,10 @@ const con = require("../lib/dbConnection");
 const jwt = require("jsonwebtoken");
 const middleware = require("../middleware/auth");
 const bcrypt = require("bcryptjs");
+const authController = require("../controller/Auth/index");
 
 // GET ALL USERS
-router.get("/", middleware, (req, res) => {
+router.get("/", (req, res) => {
   try {
     con.query("SELECT * FROM users", (err, result) => {
       if (err) throw err;
@@ -102,108 +103,16 @@ router.delete("/:id", (req, res) => {
 // REGISTER ROUTE
 // The Route where Encryption starts
 router.post("/register", (req, res) => {
-  try {
-    let sql = "INSERT INTO users SET ?";
-    const {
-      full_name,
-      email,
-      password,
-      user_type,
-      phone,
-      country,
-      billing_address,
-      default_shipping_address,
-    } = req.body;
-
-    // The start of hashing / encryption
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password, salt);
-
-    let user = {
-      full_name,
-      email,
-      // We sending the hash value to be stored witin the table
-      password: hash,
-      user_type,
-      phone,
-      country,
-      billing_address,
-      default_shipping_address,
-    };
-    con.query(sql, user, (err, result) => {
-      if (err) throw err;
-      console.log(result);
-      res.send(`User ${(user.full_name, user.email)} created successfully`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  return authController.Register(req, res);
 });
 
 // LOGIN
 router.post("/login", (req, res) => {
-  try {
-    let sql = "SELECT * FROM users WHERE ?";
-    let user = {
-      email: req.body.email,
-    };
-    con.query(sql, user, async (err, result) => {
-      if (err) throw err;
-      if (result.length === 0) {
-        res.send("Email not found please register");
-      } else {
-        const isMatch = await bcrypt.compare(
-          req.body.password,
-          result[0].password
-        );
-        if (!isMatch) {
-          res.send("Password incorrect");
-        } else {
-          // The information the should be stored inside token
-          const payload = {
-            user: {
-              user_id: result[0].user_id,
-              full_name: result[0].full_name,
-              email: result[0].email,
-              user_type: result[0].user_type,
-              phone: result[0].phone,
-              country: result[0].country,
-              billing_address: result[0].billing_address,
-              default_shipping_address: result[0].default_shipping_address,
-            },
-          };
-          // Creating a token and setting expiry date
-          jwt.sign(
-            payload,
-            process.env.jwtSecret,
-            {
-              expiresIn: "365d",
-            },
-            (err, token) => {
-              if (err) throw err;
-              res.json({ token });
-            }
-          );
-        }
-      }
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  return authController.Login(req, res);
 });
 // VERIFY
 router.get("/users/verify", (req, res) => {
-  const token = req.header("x-auth-token");
-  jwt.verify(token, process.env.jwtSecret, (error, decodedToken) => {
-    if (error) {
-      res.status(401).json({
-        msg: "Unauthorized Access!",
-      });
-    } else {
-      res.status(200);
-      res.send(decodedToken);
-    }
-  });
+  return authController.Verify(req, res);
 });
 
 module.exports = router;
